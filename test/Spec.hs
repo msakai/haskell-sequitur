87f89a93de324fa9cbb39d1b26d452dee5abf2dd
Masahiro Sakai <masahiro.sakai@gmail.com>
2024-07-13 11:11:35 +0900

    test digram uniqueness

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,10 @@
 import Control.Monad
+import qualified Data.Map.Strict as Map
 import qualified Data.IntMap.Strict as IntMap
 import Data.List (intercalate)
+import qualified Data.Set as Set
 import Test.Hspec
+import Test.QuickCheck
 
 import Language.Grammar.Sequitur
 
@@ -29,3 +32,17 @@ main = hspec $ do
     forM_ cases $ \(xs, grammar) -> do
       it ("returns " ++ reprGrammar grammar ++ " for " ++ show xs) $ do
         buildGrammar xs `shouldBe` grammar
+
+    it "returns the grammer with digram uniqueness property" $
+      property $ \s ->
+        let g = buildGrammar s
+            occurrences = Map.fromListWith Set.union [(digram, Set.singleton (i,j)) | (i, body) <- IntMap.toList g, (j, digram) <- zip [(0::Int)..] (zip body (tail body))]
+            f ps = case Set.toList ps of
+                     [_] -> True
+                     [(i1, j1), (i2, j2)] -> i1 == i2 && (j1 == j2 + 1 || j2 == j1 + 1)
+                     _ -> False
+         in counterexample (reprGrammar g) $
+            conjoin
+            [ counterexample (show ce) $ f ps
+            | ce@(_digram, ps) <- Map.toList occurrences
+            ]
