4f68c27fcb8a9f4f3136cf262fe76ec97908dc66
Masahiro Sakai <masahiro.sakai@gmail.com>
2024-07-13 20:12:42 +0900

    clean up tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,11 +11,7 @@ import Language.Grammar.Sequitur
 main :: IO ()
 main = hspec $ do
   describe "Sequitur.encode" $ do
-    let reprGrammar grammar = "{" ++ intercalate ", " [show nt ++ " -> " ++ intercalate " " (map reprSymbol body) | (nt, body) <- IntMap.toAscList grammar] ++ "}"
-           where
-             reprSymbol (Terminal c) = [c]
-             reprSymbol (NonTerminal x) = show x
-        cases =
+    let cases =
           [ ( "abab"
             , IntMap.fromList [(0, [NonTerminal 1, NonTerminal 1]), (1, [Terminal 'a', Terminal 'b'])]
             )
@@ -36,32 +32,49 @@ main = hspec $ do
       it ("returns " ++ reprGrammar grammar ++ " for " ++ show xs) $ do
         encode xs `shouldBe` grammar
 
-    it "can be inverted" $
+    it "returns a grammer with digram uniqueness property" $
+      property $ forAll simpleString $ \s ->
+        let g = encode s
+         in counterexample (reprGrammar g) $ digramUniqueness g
+
+    it "returns a grammer with rule utility property" $
+      property $ forAll simpleString $ \s ->
+        let g = encode s
+         in counterexample (reprGrammar g) $ ruleUtility g
+
+  describe "Sequitur.decode" $ do
+    it "is the inverse of encode" $
       property $ forAll simpleString $ \s ->
         let g = encode s
             s' = decode g
          in counterexample (reprGrammar g) $ counterexample s' $ s == s'
 
-    it "returns a grammer with digram uniqueness property" $
-      property $ forAll simpleString $ \s ->
-        let g = encode s
-            occurrences = Map.fromListWith Set.union [(digram, Set.singleton (i,j)) | (i, body) <- IntMap.toList g, (j, digram) <- zip [(0::Int)..] (zip body (tail body))]
-            f ps = case Set.toList ps of
-                     [_] -> True
-                     [(i1, j1), (i2, j2)] -> i1 == i2 && (j1 == j2 + 1 || j2 == j1 + 1)
-                     _ -> False
-         in counterexample (reprGrammar g) $
-            conjoin
-            [ counterexample (show ce) $ f ps
-            | ce@(_digram, ps) <- Map.toList occurrences
-            ]
-
-    it "returns a grammer with rule utilization property" $
-      property $ forAll simpleString $ \s ->
-        let g = encode s
-            occurrences = IntMap.fromListWith (+) [(r, (1::Int)) | body <- IntMap.elems g, NonTerminal r <- body]
-         in counterexample (reprGrammar g) $
-            conjoin [counterexample (show (r, n)) $ n >= 2 | (r, n) <- IntMap.toList occurrences]
-
 simpleString :: Gen String
 simpleString = liftArbitrary (elements ['a'..'z'])
+
+reprGrammar :: Grammar Char -> String
+reprGrammar grammar = "{" ++ intercalate ", " [show nt ++ " -> " ++ intercalate " " (map reprSymbol body) | (nt, body) <- IntMap.toAscList grammar] ++ "}"
+  where
+    reprSymbol (Terminal c) = [c]
+    reprSymbol (NonTerminal x) = show x
+
+digramUniqueness :: Grammar Char -> Property
+digramUniqueness g = conjoin
+  [ counterexample (show ce) $
+      case Set.toList ps of
+        [_] -> True
+        [(i1, j1), (i2, j2)] -> i1 == i2 && (j1 == j2 + 1 || j2 == j1 + 1)
+        _ -> False
+  | ce@(_digram, ps) <- Map.toList occurrences
+  ]
+  where
+    occurrences = Map.fromListWith Set.union
+      [ (digram, Set.singleton (i,j))
+      | (i, body) <- IntMap.toList g, (j, digram) <- zip [(0::Int)..] (zip body (tail body))
+      ]
+
+ruleUtility :: Grammar Char -> Property
+ruleUtility g = conjoin [counterexample (show (r, n)) $ n >= 2 | (r, n) <- IntMap.toList occurrences]
+  where
+    occurrences = IntMap.fromListWith (+)
+      [(r, (1::Int)) | body <- IntMap.elems g, NonTerminal r <- body]
